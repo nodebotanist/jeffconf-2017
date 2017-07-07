d9dfa73f61514e50b019db9163a7fa615568ac7b
Kas Perch <the@nodebotani.st>
2017-07-07 14:02:58 +0100

    Some fixed Edison code.

---
+++ code/edison-demo/bot/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const five = require("johnny-five")
+const Edison = require("edison-io")
+const board = new five.Board({
+  io: new Edison()
+})
+
+board.on("ready", function() {
+  let keypad = new five.Sensor({
+    pin: 'A0',
+    freq: 200,
+    threshold: 30
+  })
+
+  let led = new five.Led.RGB({
+    isAnode: true,
+    pins: [3, 5, 6]
+  })
+
+  let voteQueue = []
+
+  keypad.on('change', function(){
+    if(this.value < 424){
+      return
+    } else if (this.value < 750) {
+      voteQueue.push('purple')
+      led.color('#663399')
+    } else {
+      voteQueue.push('orange')
+      led.color('orange')
+    }
+  })
+
+})
